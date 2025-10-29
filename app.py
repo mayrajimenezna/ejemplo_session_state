3bc956653c65057ddffd8bdea96c6e90ac80749c
mayrajimenezna <a385869@uach.mx>
2025-10-29 08:05:04 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 st.title("Ejemplo para usar Session State")
 
 if 'count' not in st.session_state:
-  st.session_state['key'] = 0
+  st.session_state['count'] = 0
 
 
 st.write(st.session_state)
